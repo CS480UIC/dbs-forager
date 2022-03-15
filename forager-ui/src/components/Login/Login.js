9276dd8a493714d8efe48387e25fb9d15dc657f6
Prabhu <anishprabh1997@gmail.com>
2022-03-15 12:41:51 -0500

    added donor component

--- forager-ui/src/components/Login/Login.js
+++ forager-ui/src/components/Login/Login.js
@@ -36,18 +36,18 @@ function Login(props) {
             <div style={{ margin: "bottom" }}>
               <div>
                 <img src={email} alt="email" className="email" />
-                <input type="text" placeholder="username" className="name" onChange={userNameChangeHandler} />
+                <input type="text" placeholder="username" className="login-input name" onChange={userNameChangeHandler} />
               </div>
               <div className="second-input">
                 <img src={pass} alt="pass" className="email" />
-                <input type="password" placeholder="password" className="name" onChange={passwordChangeHandler} />
+                <input type="password" placeholder="password" className="login-input name" onChange={passwordChangeHandler} />
               </div>
               <div className="buttonContainer">
                 <div className="login-button">
-                  <button type="Submit">Login</button>
+                  <button className='button' type="Submit">Login</button>
                 </div>
                 <div className="login-button">
-                  <button onClick={props.signUpHandler}>Sign Up</button>
+                  <button className='button' onClick={props.signUpHandler}>Sign Up</button>
                 </div>
               </div>
               {props.login && <div className="errorMessageContainer">{props.response.data}</div>}
