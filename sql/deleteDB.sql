398ab2e9c2b991fe81de360ca06c577a360c641a
Prabhu <anishprabh1997@gmail.com>
2022-03-30 23:06:56 -0500

    addding delete sql

---
+++ sql/deleteDB.sql
@@ -0,0 +1,10 @@
+
+DROP TABLE interaction;
+DROP TABLE message;
+DROP TABLE restaurant;
+DROP TABLE users;
+DROP TABLE donor;
+DROP TABLE receiver;
+DROP TABLE food_order;
+DROP TABLE food;
+DROP TABLE volunteer;
