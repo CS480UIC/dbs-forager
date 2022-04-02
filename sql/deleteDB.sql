b4732433b8a5a97833f6eaebcc9be658cec8dacc
Pradhan B Suresh <60877349+Pradhan1297@users.noreply.github.com>
2022-04-01 22:43:50 -0500

    Create deleteDB.sql

---
+++ sql/deleteDB.sql
@@ -0,0 +1,9 @@
+DROP TABLE interaction;
+DROP TABLE message;
+DROP TABLE restaurant;
+DROP TABLE users;
+DROP TABLE donor;
+DROP TABLE receiver;
+DROP TABLE food_order;
+DROP TABLE food;
+DROP TABLE volunteer;
